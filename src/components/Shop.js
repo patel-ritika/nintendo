402e3425b2f2247a222f87273205747dd605ce08
Your Name <you@example.com>
2024-12-13 12:07:28 +0530

    add(13/12/24)

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ShopTop from './shop/ShopTop';
 import ShopTrial from './shop/ShopTrial';
+import HomeDigital from './home/HomeDigital';
 
 const Shop = () => {
   return (
     <div>
       <ShopTop/>
       <ShopTrial/>
+      <HomeDigital/>
     </div>
   );
 }
